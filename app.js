819b1f7ab620ad4a6e2096cdc2840eaf39774066
Nina-a <nina.leduc@gmail.com>
2021-04-01 11:50:20 +0200

    changement de direction

--- app.js
+++ app.js
@@ -8,21 +8,20 @@ var speedx = 0;
 // Speed on y
 var speedy = 10;
 
-// ---------------------Création du serpent-----------------------------
 let snake = [{x:140, y:150},{x:130, y:150},{x:120, y:150},{x:110, y:150}];
 
-function Animation(){
+function animation(){
     setTimeout(function(){
-        CleanCanvas();
-        MoveForwardSnake();
-        DrawSnake();
-        Animation();
-    },100)
+        cleanCanvas();
+        moveForwardSnake();
+        drawSnake();
+        animation();
+    },1000)
 }
 
-Animation();
+animation();
 
-function CleanCanvas(){
+function cleanCanvas(){
     // Canvas's color
     contexte.fillStyle = "white";
     // Stroke's color
@@ -32,29 +31,55 @@ function CleanCanvas(){
     contexte.strokeRect(0,0,canvas.width, canvas.height);
 }
 
-function DrawPiecesSnake(piece){
+function drawPiecesSnake(piece){
     contexte.fillStyle = "#00fe14"
     contexte.strokeStyle = "black"
     contexte.fillRect(piece.x, piece.y, 10, 10);
     contexte.strokeRect(piece.x, piece.y, 10, 10);
 }
 
-function DrawSnake(){
+function drawSnake(){
     snake.forEach(piece => {
-        DrawPiecesSnake(piece);
+        drawPiecesSnake(piece);
     })
 }
 
 // ---------------------Création du serpent-----------------------------
-function MoveForwardSnake() {
+function moveForwardSnake() {
     const head = {x: snake[0].x + speedx, y: snake[0].y -speedy}
     snake.unshift(head);
     snake.pop();
 }
 
-MoveForwardSnake();
-MoveForwardSnake();
-MoveForwardSnake();
-DrawSnake();
+drawSnake();
+
+document.addEventListener("keydown", changeDirection)
+
+function changeDirection(event){
+    console.log(event);
+
+    // Constante permettant d'éviter de faire un demi-tour
+    const enTrainDeMonter = speedy === -10;    
+    const enTrainDeDescendre = speedy === 10;
+    const enTrainDAllerADroite = speedx === 10;
+    const EnTrainDAllerAGauche = speedx === -10;
+
+    if (event.code === "ArrowUp" && !enTrainDeDescendre){
+        speedy = 10;
+        speedx = 0;
+    }
+    else if(event.code === "ArrowDown" && !enTrainDeMonter){
+        speedy = -10;
+        speedx = 0;
+    }
+    else if(event.code === "ArrowLeft" && !enTrainDAllerADroite){
+        speedy = 0;
+        speedx = -10;       
+    }
+    else if(event.code === "ArrowRight" && !EnTrainDAllerAGauche){
+        speedy = 0;
+        speedx = 10;        
+    }
+}
 
 
